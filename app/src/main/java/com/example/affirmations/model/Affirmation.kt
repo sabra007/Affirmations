94b5e990eb5f4f49da90becaab939b1b3dfee9f7
sabra007 <sargis.ab@GMAIL.COM>
2021-09-22 17:33:13 -0700

    add images and new icon

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,5 +1,10 @@
 package com.example.affirmations.model
 
-data class Affirmation(val stringResourceId: Int) {
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+
+// To avoid passing arguments in a wrong order we use resource annotations( Start with @)
+data class Affirmation(@StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int) {
 
 }
